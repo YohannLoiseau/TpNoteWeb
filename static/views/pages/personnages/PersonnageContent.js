cfbbad4ac6efd122067e429fc93bfb2f4abc4c5e
Kevin-LB <le.breton.kevin@icloud.com>
2024-03-26 10:15:43 +0100

    ajout de toutes les info des perso

--- static/views/pages/personnages/PersonnageContent.js
+++ static/views/pages/personnages/PersonnageContent.js
@@ -21,9 +21,17 @@ export default class PersonnageContent {
                         <img src="${personnage.icon}" alt="${personnage.name} Icon" class="personnage-icon">
                         <p class="personnage-description">${personnage.description}</p>
                         <p class="personnage-tags">Tags : ${personnage.tags.join(', ')}</p>
-                        <p class="personnage-stats">HP : ${personnage.stats.hp}</p>
-                        <p class="personnage-stats">MP : ${personnage.stats.mp}</p>
-                        <!-- Ajoutez d'autres détails du personnage ici -->
+                        <p class="personnage-hp">HP : ${personnage.stats.hp}</p>
+                        <p class="personnage-mp">MP : ${personnage.stats.mp}</p>
+                        <p class="personnage-movespeed">Movespeed : ${personnage.stats.movespeed}</p>
+                        <p class="personnage-armor">Armor : ${personnage.stats.armor}</p>
+                        <p class="personnage-spellblock">Spellblock : ${personnage.stats.spellblock}</p>
+                        <p class="personnage-attackrange">Attack Range : ${personnage.stats.attackrange}</p>
+                        <p class="personnage-hpregen">HP Regen : ${personnage.stats.hpregen}</p>
+                        <p class="personnage-mpregen">MP Regen : ${personnage.stats.mpregen}</p>
+                        <p class="personnage-crit">Crit : ${personnage.stats.crit}</p>
+                        <p class="personnage-attackdamage">Attack Damage : ${personnage.stats.attackdamage}</p>
+                        <p class="personnage-attackspeed">Attack Speed : ${personnage.stats.attackspeed}</p>
                     </div>
                 </div>
                 <p><a href="#">Revenir à l'accueil</a></p>
